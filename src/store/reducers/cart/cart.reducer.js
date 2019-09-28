f708cc4299bf9044b3d1bd8a50411db169d6bf41
Joel Thierry NTEBE <ntebej@gmail.com>
2019-09-28 23:56:40 +0100

    Added addItem and removeItem functionnality to CheckoutItem component

--- src/store/reducers/cart/cart.reducer.js
+++ src/store/reducers/cart/cart.reducer.js
@@ -1,6 +1,11 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./cart.types";
+import {
+  TOGGLE_CART_HIDDEN,
+  ADD_ITEM,
+  CLEAR_ITEM_FROM_CART,
+  REMOVE_ITEM
+} from "./cart.types";
 
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -19,6 +24,20 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+
+    case CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
+
+    case REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemToCart(state.cartItems, action.payload)
       };
 
     default:
